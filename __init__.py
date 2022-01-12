a57ed7ec8b40c9572adc3e5372de36e2f1fd59c4
saket2001 <saketchandorkar123@gmail.com>
2022-01-12 12:27:53 +0530

    version 1.5 done

--- __init__.py
+++ __init__.py
@@ -4,7 +4,6 @@ from flask_restful import Api
 from dotenv import load_dotenv
 from authlib.integrations.flask_client import OAuth
 from models import UserInfo
-import os
 from flask_hashing import Hashing
 import helperFunc
 
@@ -17,8 +16,8 @@ def create_app():
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
         SQLALCHEMY_POOL_RECYCLE=299,
         SQLALCHEMY_POOL_TIMEOUT=20,
-        SQLALCHEMY_DATABASE_URI='mysql://AlegriaTheFest:2022themeisvintwood@AlegriaTheFest.mysql.pythonanywhere-services.com/AlegriaTheFest$alegria2022',
-        # SQLALCHEMY_DATABASE_URI='mysql://root:root@localhost/alegria_web',
+        # SQLALCHEMY_DATABASE_URI='mysql://AlegriaTheFest:2022themeisvintwood@AlegriaTheFest.mysql.pythonanywhere-services.com/AlegriaTheFest$alegria2022',
+        SQLALCHEMY_DATABASE_URI='mysql://root:root@localhost/alegria_web',
         MAIL_SERVER='smtp.gmail.com',
         MAIL_PORT=465,
         MAIL_USE_SSL=True,
@@ -176,5 +175,10 @@ def create_app():
 
     # enable csrf
     csrf.init_app(app)
+    if __name__ == "__main__":
+        app.run(debug=True)
 
     return app
+
+
+create_app()
