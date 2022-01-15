2a1a450dd8c5cdea150bc7d5ed818d519da378ec
saket2001 <saketchandorkar123@gmail.com>
2022-01-15 21:27:10 +0530

    added fallback to loading

--- static/js/loading.js
+++ static/js/loading.js
@@ -1,6 +1,11 @@
 // hides loader
-
 window.onload = (e) => {
   document.querySelector(".loading-bg").classList.add("hide");
   document.querySelector(".loader").classList.add("hide");
 };
+
+// fallback
+setTimeout(() => {
+  document.querySelector(".loading-bg").classList.add("hide");
+  document.querySelector(".loader").classList.add("hide");
+}, 15000);
